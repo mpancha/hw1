69236b5da26523ae1f46b41c1f592e6085a7e2d7
Mitul Mahendrakumar Panchal <mpancha@ncsu.edu>
2015-09-14 23:49:08 -0400

    python based deployment

--- deploy_nginx.py
+++ deploy_nginx.py
@@ -1,128 +1,138 @@
 #!/usr/bin/python
-
+import sys
 import boto.ec2
 import subprocess
 import ansible.runner
 from ansible.inventory import Inventory
+from ansible.playbook import PlayBook
+from ansible import utils
+from ansible import callbacks
 import digitalocean
 import time
 import webbrowser
 
-aws_access = 'AKIAI6HVXS3JHOZYQF6Q'
-aws_secret = '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'
-digital_token = "22e87341d8662ea4d49d35dc2fcaccf6a6e50d7e43bb486509e50a1639d3e234"
-droplet=''
-aws_con=''
-def create_aws_instance():
-   ''' use access key and '''
-   aws_con = boto.ec2.connect_to_region("us-east-1")
-   aws_con.run_instances('ami-d05e75b8',key_name='aws_hw1',instance_type='t2.micro', security_groups=['hw1'])
+class deployment:
+   """Deployment class """
+   def __init__(self):
+      self.aws_access = 'AKIAI6HVXS3JHOZYQF6Q'
+      self.aws_secret = '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'
+      self.digital_token = "22e87341d8662ea4d49d35dc2fcaccf6a6e50d7e43bb486509e50a1639d3e234"
+      self.droplet=''
+      self.aws_con=''
+      self.aws_image = 'ami-d05e75b8'
+      self.aws_size = 't2.micro'
+      
+   def create_aws_instance(self):
+      ''' use access key and '''
+      self.aws_con = boto.ec2.connect_to_region("us-east-1")
+      self.aws_con.run_instances(self.aws_image,key_name='aws_hw1',instance_type=self.aws_size, security_groups=['hw1'])
 
-def create_digital_instance():
-    droplet = digitalocean.Droplet(token=digital_token, 
+   def create_digital_instance(self):
+      self.droplet = digitalocean.Droplet(token=self.digital_token, 
                                    name='hw1droplet', 
-                                   region='nyc2', 
+                                   region='nyc1', 
                                    image='ubuntu-14-04-x64', 
                                    size_slug='512mb',
 				   ssh_keys = [1271814])
-    droplet.create()
-    print droplet
-    print "Droplet Created"
+      self.droplet.create()
+      print "Droplet Created", self.droplet
 
-def destroy_digital_instance():
-    manager = digitalocean.Manager(digital_token)
-    droplets = manager.get_all_droplets()
-    for drop in droplets:
-         drop.shutdown()
+   def destroy_aws_instance(self):
+      self.con = boto.ec2.connect_to_region("us-east-1")
+      reservations = self.con.get_all_reservations()
+      for res in reservations:
+         for instance in res.instances:
+            instance.terminate()
+            #print "instance terminated"
+    
+   def destroy_digital_instance(self):
+      manager = digitalocean.Manager(token=self.digital_token)
+      droplets = manager.get_all_droplets()
+      for drop in droplets:
+         try:
+             drop.destroy()
+             #print "droplet destroyed"
+         except:
+             print "Already destroyed"
 
-#print "Creating instance"
-#create_instance()
-dns_name=''
-def get_aws_reservation():
-   con = boto.ec2.connect_to_region("us-east-1")
-   reservations = con.get_all_reservations()
-   print reservations
-   for res in reservations:
-       print res
-       for instance in res.instances:
-          print instance
-          print instance.state
-          if instance.state == "running":
-             return instance.ip_address
-   return None
+   def get_aws_reservation(self):
+      con = boto.ec2.connect_to_region("us-east-1")
+      reservations = con.get_all_reservations()
+      #print reservations
+      for res in reservations:
+         #print res
+         for instance in res.instances:
+            #print instance
+            #print instance.state
+            if instance.state == "running":
+               return instance.ip_address
+      return None
 
-def get_digital_reservation():
-   manager = digitalocean.Manager(token=digital_token)
-   droplets = manager.get_all_droplets()
-   for droplet in droplets:
-      actions = droplet.get_actions()
-      for action in actions:
-         action.load()
-         if action.status == "completed":
-              return droplet.ip_address
-   return None
+   def get_digital_reservation(self):
+      manager = digitalocean.Manager(token=self.digital_token)
+      droplets = manager.get_all_droplets()
+      for droplet in droplets:
+         #print droplet.status
+         if droplet.id == self.droplet.id and droplet.status=="active":
+            actions = droplet.get_actions()
+            for action in actions:
+               action.load()
+               if action.status == "completed":
+                  return droplet.ip_address
+      return None
 
-def run_script_on_instance():
-   cmd = "ssh -i hw1.pem ubuntu@"+dns_name+" myscript.sh"
-   subprocess.check_output(cmd)
-
-#print "Get reservation"
-#get_reservation()
-#print "Run script"
-#run_script_on_instance()
-# create inventory file
-print "Creating digitalocean droplet"
-create_digital_instance()
-print "Creating AWS EC2 instance"
-create_aws_instance()
-dropletIp=None
-while dropletIp == None:
-   time.sleep(30)
-   dropletIp = get_digital_reservation()
-   print "Droplet not ready, retry after 30 sec" 
-print "Droplet IP ="+dropletIp
-aws_ip=None
-while aws_ip == None:
-   time.sleep(30)
-   aws_ip = get_aws_reservation()
-   print "AWS Instance not ready, retry after 30 sec"
-print "AWS instance IP =" + aws_ip
+def main(argv):
+   d = deployment()
+   print "Clean up stale reservations...*****************\n"
+   d.destroy_aws_instance()
+   d.destroy_digital_instance()
+   if len(argv)>1:
+      exit(1)
+   print "\nCreating digitalocean droplet...**************"
+   d.create_digital_instance()
+   print "\nCreating AWS EC2 instance...******************"
+   d.create_aws_instance()
+   print "\nCheck Droplet status...***********************"
+   dropletIp = d.get_digital_reservation()
+   while dropletIp == None:
+      print "Droplet not ready, will retry after 30 sec" 
+      time.sleep(30)
+      dropletIp = d.get_digital_reservation()
+   print "Droplet IP ="+dropletIp
+   print "\nCheck AWS instance status...******************"
+   aws_ip = d.get_aws_reservation()
+   while aws_ip == None:
+      print "AWS Instance not ready, retry after 30 sec"
+      time.sleep(30)
+      aws_ip = d.get_aws_reservation()
+   print "AWS instance IP =" + aws_ip
    
-digital_inv = "droplet ansible_ssh_host="+dropletIp+" ansible_ssh_user=root ansible_ssh_private_key_file=./keys/hw1.key\n"
-aws_inv = "aws ansible_ssh_host="+aws_ip+" ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./keys/aws_hw1.key"
-print digital_inv
-print aws_inv
-with open("inventory","w") as f:
-    f.write(digital_inv)
-    f.write(aws_inv)
-print "Inventory updated"
+   digital_inv = "droplet ansible_ssh_host="+dropletIp+" ansible_ssh_user=root ansible_ssh_private_key_file=./keys/hw1.key\n"
+   aws_inv = "aws ansible_ssh_host="+aws_ip+" ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./keys/aws_hw1.key"
+   
+   print "\nWriting Inventory...**************************"
+   with open("inventory","w") as f:
+      f.write(digital_inv)
+      f.write(aws_inv)
 
-runner = ansible.runner.Runner(
-   module_name='ping',
-   module_args='',
-   pattern='*',
-   forks=10,
-   inventory=Inventory('inventory')
-)
-nginx_install = ansible.runner.Runner(
-   module_name='apt',
-   module_args='pkg=nginx state=installed update_cache=true',
-   pattern='*',
-   forks=10,
-   inventory=Inventory('inventory')
-)
-nginx_start = ansible.runner.Runner(
-   module_name='shell',
-   module_args='nginx',
-   pattern='*',
-   forks=10,
-   inventory=Inventory('inventory')
-)
-print "Run ansible task: nginx_install"
-datastructure = nginx_install.run()
-#print( datastructure )
-print "Run ansible task: nginx_start"
-datastructure = nginx_start.run()
-#print( datastructure )
-webbrowser.open_new_tab("http://"+aws_ip)
-webbrowser.open_new_tab("http://"+dropletIp)
+   utils.VERBOSITY = 0
+   playbook_cb = callbacks.PlaybookCallbacks(verbose=utils.VERBOSITY)
+   stats = callbacks.AggregateStats()
+   runner_cb = callbacks.PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY)
+   inventory = Inventory('inventory')
+
+   time.sleep(30)
+   print "\nRun Ansible PlayBook...**********************"
+   pb = PlayBook(playbook='server_play.yml',
+              inventory=inventory,
+              callbacks=playbook_cb,
+              runner_callbacks=runner_cb,
+              stats=stats
+        )
+   pb.run()	
+   print "\nOpening web browser...***********************"
+   webbrowser.open_new_tab("http://"+aws_ip)
+   webbrowser.open_new_tab("http://"+dropletIp)
+
+if __name__=="__main__":
+   main(sys.argv)
